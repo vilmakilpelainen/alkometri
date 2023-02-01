67f35b81f399622d80cc581dbb6af3c3215ce7b0
vilmakilpelainen <n2kivi00@students.oamk.fi>
2023-02-01 13:44:20 +0200

    alkometri

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react"
 
 function App() {
+  const [weight, setWeight] = useState(0);
+  const [gender, setGender] = useState("male");
+  const [bottles, setBottles] = useState(0);
+  const [time, setTime] = useState(0);
+  const [result, setResult] = useState(0);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const litres = bottles * 0.33;
+    const grams = litres * 8 * 4.5;
+    const burning = weight / 10;
+    const gramsLeft = grams - (burning * time);
+    let result = 0
+    if (gender === "male") {
+      result = gramsLeft/ (weight * 0.7);
+    } else {
+      result = gramsLeft/ (weight * 0.6);
+    }
+    setResult(result);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h3>Alcometer</h3>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Weight </label>
+        <input name="weight" type="number" step="1" value={weight} onChange={e => setWeight(e.target.value)}></input>
+      </div>
+      <div>
+        <label>bottles </label>
+        <input name="bottle" type="number" value={bottles} onChange={e => setBottles(e.target.value)}></input>
+      </div>
+      <div>
+        <label>Gender </label>
+        <input type="radio" name="gender" value="male" defaultChecked onChange={e => setGender(e.target.value)}/><label>Male</label>
+        <input type="radio" name="gender" value="female" onChange={e => setGender(e.target.value)} /><label>Female</label>
+      </div>
+      <div>
+        <label>Time </label>
+        <input type="number" name="time" value={time} onChange={e => setTime(e.target.value)}></input>
+    
+        <div><output>{result.toFixed(2)}</output></div>
+      </div>
+      <button>Calculate </button>
+    </form>
+    </>
+   
   );
-}
+  };
+  
+   
 
 export default App;
